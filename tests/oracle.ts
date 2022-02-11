38bbae53343ae05232c2292ec910baac97f67a55
Olivier EBLÉ <olivier@eble.fr>
2022-02-11 10:51:21 +0100

    WIP now tests can talk with program

--- tests/oracle.ts
+++ tests/oracle.ts
@@ -1,8 +1,9 @@
 require('dotenv').config();
-import { Keypair, PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY } from '@solana/web3.js';
+import { Keypair, PublicKey, SystemProgram, SYSVAR_CLOCK_PUBKEY, Connection, ConnectionConfig } from '@solana/web3.js';
 import { strictEqual } from 'assert';
 import * as fs from "fs";
-import { Provider, Program, setProvider, workspace, BN } from "@project-serum/anchor"
+import { Provider, Program, setProvider, workspace, BN, Wallet} from "@project-serum/anchor"
+import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet';
 
 enum Tokens {
     SOL = 1,
@@ -18,10 +19,19 @@ describe("oracle", () => {
     const keypair_acc = Uint8Array.from(Buffer.from(JSON.parse(require('fs').readFileSync(`./keys/${process.env.CLUSTER}/owner.json`))));
     const admin = Keypair.fromSecretKey(keypair_acc);
 
+    let config: ConnectionConfig = {
+        commitment: Provider.defaultOptions().commitment,
+        confirmTransactionInitialTimeout: 220000,
+      };
+
+    const connection = new Connection('http://127.0.0.1:8899', config);
+    const wallet = new NodeWallet(admin);
+    const provider = new Provider(connection, wallet, Provider.defaultOptions());
+    const initialMarketOwner = provider.wallet.publicKey;
+    setProvider(provider);
+    
     const idl = JSON.parse(fs.readFileSync("./target/idl/oracle.json", "utf8"));
     const programId = new PublicKey('A9DXGTCMLJsX7kMfwJ2aBiAFACPmUsxv6TRxcEohL4CD');
-    const provider = Provider.local()
-    setProvider(provider);
     const program = new Program(idl, programId);
 
 
@@ -42,28 +52,25 @@ describe("oracle", () => {
         });
 
         {
-            let oracle = await program.account.oracle.fetch(oracleAccount.publicKey);
+            let oracle = await program.account.oraclePrices.fetch(oracleAccount.publicKey);
             console.log("Oracle", oracle);
         }
 
-        // let updatedSolPrice = 20;
-        // await program.rpc.update(
-        //     new BN(3),      // SRM
-        //     new BN(updatedSolPrice), {
-        //     accounts: {
-        //         admin: admin.publicKey,
-        //         oracle: oracleAccount.publicKey,
-        //         clock: SYSVAR_CLOCK_PUBKEY
-        //     },
-        //     signers: [admin]
-        // });
-
-        // {
-        //     let oracle: any = await program.account.oracle.fetch(
-        //         oracleAccount.publicKey
-        //       );
-        //     console.log("Oracle", oracle);
-        //     strictEqual(oracle.srm.price.toNumber(), updatedSolPrice);
-        // }
+        let updatedSolPrice = 20;
+        await program.rpc.update(
+            new BN(3),      // SRM
+            new BN(updatedSolPrice), {
+            accounts: {
+                admin: admin.publicKey,
+                oracle: oracleAccount.publicKey,
+                clock: SYSVAR_CLOCK_PUBKEY
+            },
+            signers: [admin]
+        });
+        {
+            let oracle = await program.account.oraclePrices.fetch(oracleAccount.publicKey);
+            console.log("Oracle", oracle);
+            strictEqual(oracle.srm.price.toNumber(), updatedSolPrice);
+        }
     });
 });
