99c3f638f405dea3786bef1cfba305bc3b041d55
Olivier EBLÉ <olivier@eble.fr>
2022-02-18 10:24:09 +0100

    Clean-up pyth product validation

--- programs/oracle/src/utils/pyth.rs
+++ programs/oracle/src/utils/pyth.rs
@@ -1,6 +1,4 @@
-#![allow(dead_code)] //TODO: remove this
-
-use crate::{Price, Result, ScopeError, Token};
+use crate::{Price, Result, ScopeError};
 use anchor_lang::prelude::*;
 use pyth_client::{PriceStatus, PriceType};
 use std::convert::{TryFrom, TryInto};
@@ -47,60 +45,6 @@ fn get_status(st: &PriceStatus) -> bool {
     matches!(st, PriceStatus::Trading)
 }
 
-pub fn validate_pyth_product(pyth_product: &pyth_client::Product) -> ProgramResult {
-    if pyth_product.magic != pyth_client::MAGIC {
-        msg!("Pyth product account provided is not a valid Pyth account");
-        return Err(ProgramError::InvalidArgument);
-    }
-    if pyth_product.atype != pyth_client::AccountType::Product as u32 {
-        msg!("Pyth product account provided is not a valid Pyth product account");
-        return Err(ProgramError::InvalidArgument);
-    }
-    if pyth_product.ver != pyth_client::VERSION_2 {
-        msg!("Pyth product account provided has a different version than the Pyth client");
-        return Err(ProgramError::InvalidArgument);
-    }
-    if !pyth_product.px_acc.is_valid() {
-        msg!("Pyth product price account is invalid");
-        return Err(ProgramError::InvalidArgument);
-    }
-
-    Ok(())
-}
-
-pub fn validate_pyth_product_symbol(
-    pyth_product: &pyth_client::Product,
-    token: &Token,
-) -> ProgramResult {
-    match read_pyth_product_attribute(pyth_product, "symbol") {
-        None => {
-            msg!("Pyth product account does not contain symbol");
-            return Err(ProgramError::InvalidArgument);
-        }
-        Some(product_symbol) => {
-            let symbol_for_token = get_pyth_symbol_for_token(token);
-            let symbol_for_token_dev = get_pyth_symbol_for_token_devnet(token);
-            if product_symbol != symbol_for_token && product_symbol != symbol_for_token_dev {
-                msg!("Pyth product account has invalid symbol. Expected: {} symbol for collateral token {:?}. Actual: {}", symbol_for_token, token, product_symbol);
-                return Err(ProgramError::InvalidArgument);
-            }
-        }
-    };
-    Ok(())
-}
-
-pub fn validate_pyth_price_pubkey(
-    pyth_product: &pyth_client::Product,
-    pyth_price_pubkey: &Pubkey,
-) -> ProgramResult {
-    if pyth_product.px_acc.val[..] != pyth_price_pubkey.to_bytes() {
-        msg!("Pyth product price account does not match the Pyth price account provided");
-        return Err(ProgramError::InvalidArgument);
-    }
-
-    Ok(())
-}
-
 pub fn validate_pyth_price(pyth_price: &pyth_client::Price) -> ProgramResult {
     if pyth_price.magic != pyth_client::MAGIC {
         msg!("Pyth price account provided is not a valid Pyth account");
@@ -121,73 +65,9 @@ pub fn validate_pyth_price(pyth_price: &pyth_client::Price) -> ProgramResult {
     Ok(())
 }
 
-pub fn read_pyth_product_attribute(
-    pyth_product: &pyth_client::Product,
-    attribute: &str,
-) -> Option<String> {
-    let mut psz = pyth_product.size as usize - pyth_client::PROD_HDR_SIZE;
-    let mut pit = (&pyth_product.attr[..]).iter();
-    while psz > 0 {
-        let key = get_attr_str(&mut pit);
-        let val = get_attr_str(&mut pit);
-        if key == attribute {
-            return Some(val);
-        }
-        psz -= 2 + key.len() + val.len();
-    }
-    None
-}
-
-fn get_attr_str<'a, T>(ite: &mut T) -> String
-where
-    T: Iterator<Item = &'a u8>,
-{
-    let mut len = *ite.next().unwrap() as usize;
-    let mut val = String::with_capacity(len);
-    while len > 0 {
-        val.push(*ite.next().unwrap() as char);
-        len -= 1;
-    }
-    val
-}
-
-macro_rules! pyth_symbol {
-    ($prefix: literal) => {
-        format!("{}/USD", $prefix)
-    };
-}
-
-pub fn get_pyth_symbol_for_token(token: &Token) -> String {
-    match token {
-        Token::SOL => pyth_symbol!("SOL"),
-        Token::ETH => pyth_symbol!("ETH"),
-        Token::BTC => pyth_symbol!("BTC"),
-        Token::SRM => pyth_symbol!("SRM"),
-        Token::RAY => pyth_symbol!("RAY"),
-        Token::FTT => pyth_symbol!("FTT"),
-        Token::MSOL => pyth_symbol!("MSOL"),
-    }
-}
-
-pub fn get_pyth_symbol_for_token_devnet(token: &Token) -> String {
-    match token {
-        Token::SOL => pyth_symbol!("Crypto.SOL"),
-        Token::ETH => pyth_symbol!("Crypto.ETH"),
-        Token::BTC => pyth_symbol!("Crypto.BTC"),
-        Token::SRM => pyth_symbol!("Crypto.SRM"),
-        Token::RAY => pyth_symbol!("Crypto.RAY"),
-        Token::FTT => pyth_symbol!("Crypto.FTT"),
-        Token::MSOL => pyth_symbol!("Crypto.MSOL"),
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use anchor_lang::prelude::{ProgramError, Pubkey};
-    use pyth_client::AccKey;
-
-    use super::utils::{new_product, new_product_attributes};
-    use crate::Token;
+    use anchor_lang::prelude::ProgramError;
 
     const PRICE_ACCT_SIZE: usize = 3312;
 
@@ -195,126 +75,6 @@ mod tests {
     const PRICE_VERSION_OFFSET: usize = 4;
     const PRICE_TYPE_OFFSET: usize = 16;
     const PRICE_STATUS_OFFSET: usize = 224;
-
-    #[test]
-    pub fn test_validate_product() {
-        assert_eq!(
-            super::validate_pyth_product(&new_product()).err().is_none(),
-            true
-        );
-    }
-
-    #[test]
-    pub fn test_validate_product_magic_number() {
-        let product = pyth_client::Product {
-            magic: 0xa1b2c3d3, // incorrect magic
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_product(&product).err().unwrap(),
-            ProgramError::InvalidArgument
-        );
-    }
-
-    #[test]
-    pub fn test_validate_product_account_type() {
-        let product = pyth_client::Product {
-            atype: pyth_client::AccountType::Mapping as u32, // incorrect atype
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_product(&product).err().unwrap(),
-            ProgramError::InvalidArgument
-        );
-    }
-
-    #[test]
-    pub fn test_validate_product_version() {
-        let product = pyth_client::Product {
-            ver: 0_u32, // incorrect ver
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_product(&product).err().unwrap(),
-            ProgramError::InvalidArgument
-        );
-    }
-
-    #[test]
-    pub fn test_validate_product_px_acc() {
-        let product = pyth_client::Product {
-            px_acc: pyth_client::AccKey {
-                val: Pubkey::default().to_bytes(), // incorrect px_acc
-            },
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_product(&product).err().unwrap(),
-            ProgramError::InvalidArgument
-        );
-    }
-
-    #[test]
-    pub fn test_validate_product_symbol() {
-        let product = pyth_client::Product {
-            attr: new_product_attributes("symbol", "ETH/USD"),
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_product_symbol(&product, &Token::ETH)
-                .err()
-                .is_none(),
-            true
-        );
-    }
-
-    #[test]
-    pub fn test_validate_product_symbol_incorrect() {
-        let product = pyth_client::Product {
-            attr: new_product_attributes("symbol", "BTC/USD"),
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_product_symbol(&product, &Token::ETH)
-                .err()
-                .unwrap(),
-            ProgramError::InvalidArgument
-        );
-    }
-
-    #[test]
-    pub fn validate_pyth_price_pubkey() {
-        let pubkey = Pubkey::new_unique();
-        let product = pyth_client::Product {
-            px_acc: AccKey {
-                val: pubkey.to_bytes(),
-            },
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_price_pubkey(&product, &pubkey)
-                .err()
-                .is_none(),
-            true
-        );
-    }
-
-    #[test]
-    pub fn validate_pyth_price_pubkey_mismatch() {
-        let pubkey = Pubkey::new_unique();
-        let product = pyth_client::Product {
-            px_acc: AccKey {
-                val: pubkey.to_bytes(),
-            },
-            ..new_product()
-        };
-        assert_eq!(
-            super::validate_pyth_price_pubkey(&product, &Pubkey::new_unique())
-                .err()
-                .unwrap(),
-            ProgramError::InvalidArgument
-        );
-    }
 
     #[test]
     pub fn test_validate_price() {
