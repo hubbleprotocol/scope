32e95b191c66e9ca1413c94d01ed0036e2d412ca
Silviu Troscot <silviu.troscot10@gmail.com>
2023-11-11 13:36:41 +0200

    fix fmt

--- programs/scope/src/handlers/mod.rs
+++ programs/scope/src/handlers/mod.rs
@@ -2,8 +2,8 @@ pub mod handler_initialize;
 pub mod handler_initialize_tokens_metadata;
 pub mod handler_refresh_prices;
 pub mod handler_update_mapping;
-pub mod handler_update_token_metadata;
 pub mod handler_update_mapping_twap;
+pub mod handler_update_token_metadata;
 
 pub use handler_initialize::*;
 pub use handler_initialize_tokens_metadata::*;
--- programs/scope/src/oracles/twap.rs
+++ programs/scope/src/oracles/twap.rs
@@ -1,22 +1,37 @@
+use decimal_wad::decimal::Decimal;
+
 use crate::{DatedPrice, OracleMappings, OracleTwaps, Price};
 
 pub fn update_twap(
+    oracle_mappings: &OracleMappings,
     oracle_twaps: &mut OracleTwaps,
     token: usize,
     price: Price,
     current_ts: u64,
     current_slot: u64,
-) -> crate::Result<Price> {
+) -> crate::Result<DatedPrice> {
     // todo: impl this to calculate and update the new twap value
-    Ok(Price::default())
+
+    let source_index = usize::from(oracle_mappings.twap_source[token]);
+
+    let twap = oracle_twaps.twaps[source_index];
+    // if there is no previous twap, store the existent
+    if twap.last_update_slot == 0 {
+        twap.current_ema_1h = Decimal::from(price).to_scaled_val().unwrap();
+        twap.last_update_slot = current_slot;
+        twap.last_update_unix_timestamp = current_ts;
+
+        return Ok(get_price(oracle_mappings, &oracle_twaps, token));
+    } else {
+    }
 }
 
 pub fn get_price(
     oracle_mappings: &OracleMappings,
     oracle_twaps: &OracleTwaps,
-    token_index: usize,
+    token: usize,
 ) -> DatedPrice {
-    let source_index = usize::from(oracle_mappings.twap_source[token_index]);
+    let source_index = usize::from(oracle_mappings.twap_source[token]);
 
     let twap = oracle_twaps.twaps[source_index];
     return twap.to_dated_price();
