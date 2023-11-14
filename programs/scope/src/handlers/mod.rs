76812c998b7af12faabd2f64e3ed11971975865d
Olivier Eblé <olivier@eble.fr>
2023-11-14 16:56:58 +0100

    Remove init twap from scope

--- programs/scope/src/handlers/mod.rs
+++ programs/scope/src/handlers/mod.rs
@@ -1,12 +1,10 @@
 pub mod handler_initialize;
-pub mod handler_initialize_oracle_twaps;
 pub mod handler_refresh_prices;
 pub mod handler_reset_twap;
 pub mod handler_update_mapping;
 pub mod handler_update_token_metadata;
 
 pub use handler_initialize::*;
-pub use handler_initialize_oracle_twaps::*;
 pub use handler_refresh_prices::*;
 pub use handler_reset_twap::*;
 pub use handler_update_mapping::*;
