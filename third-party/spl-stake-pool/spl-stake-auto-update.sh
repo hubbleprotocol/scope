68c389eb04cf30888f5a8977f683922675cf045c
Olivier Eblé <olivier@eble.fr>
2024-01-17 15:02:50 +0100

    Repeat commands for updating SplStake oracles

--- third-party/spl-stake-pool/spl-stake-auto-update.sh
+++ third-party/spl-stake-pool/spl-stake-auto-update.sh
@@ -10,3 +10,14 @@ case "$uname" in
     (*) echo 'error: unsupported platform.'; exit 2; ;;
 esac;
 
+case "$uname" in
+    (*Linux*) jq 'to_entries | map(.value) | .[]' ../../configs/mainnet/hubble.json | tail -n +2 | jq 'select(.oracle_type=="SplStake") | .oracle_mapping' | xargs -i bash -c 'spl-stake-pool update --no-merge {}'; ;;
+    (*Darwin*) jq 'to_entries | map(.value) | .[]' ../../configs/mainnet/hubble.json | tail -n +2 | jq 'select(.oracle_type=="SplStake") | .oracle_mapping' | gxargs -i bash -c 'spl-stake-pool update --no-merge {}'; ;;
+    (*) echo 'error: unsupported platform.'; exit 2; ;;
+esac;
+
+case "$uname" in
+    (*Linux*) jq 'to_entries | map(.value) | .[]' ../../configs/mainnet/hubble.json | tail -n +2 | jq 'select(.oracle_type=="SplStake") | .oracle_mapping' | xargs -i bash -c 'spl-stake-pool update {}'; ;;
+    (*Darwin*) jq 'to_entries | map(.value) | .[]' ../../configs/mainnet/hubble.json | tail -n +2 | jq 'select(.oracle_type=="SplStake") | .oracle_mapping' | gxargs -i bash -c 'spl-stake-pool update {}'; ;;
+    (*) echo 'error: unsupported platform.'; exit 2; ;;
+esac;
