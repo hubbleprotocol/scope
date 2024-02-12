f997739206b20ebf6b8f5f9c247c7c2fdda49961
Olivier Eblé <olivier@eble.fr>
2024-02-12 11:03:25 +0100

    Add SwitchboardDecimal conversion tools

--- programs/switchbord-itf/src/accounts.rs
+++ programs/switchbord-itf/src/accounts.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_lang::Discriminator;
+use rust_decimal::{prelude::FromPrimitive, Decimal};
 
 #[account(zero_copy(unsafe))]
 #[repr(packed)]
@@ -28,7 +30,7 @@ pub struct AggregatorAccountData {
     pub force_report_period: i64,
     /// Timestamp when the feed is no longer needed.
     pub expiration: i64,
-    //
+
     /// Counter for the number of consecutive failures before a feed is removed from a queue. If set to 0, failed feeds will remain on the queue.
     pub consecutive_failure_count: u64,
     /// Timestamp when the next update request will be available.
@@ -49,7 +51,7 @@ pub struct AggregatorAccountData {
     pub job_pubkeys_size: u32,
     /// Used to protect against malicious RPC nodes providing incorrect task definitions to oracles before fulfillment.
     pub jobs_checksum: [u8; 32],
-    //
+
     /// The account delegated as the authority for making account changes.
     pub authority: Pubkey,
     /// Optional, public key of a history buffer account storing the last N accepted results and their timestamps.
@@ -71,7 +73,46 @@ pub struct AggregatorAccountData {
     pub _ebuf: [u8; 138],
 }
 
+impl Default for AggregatorAccountData {
+    fn default() -> Self {
+        Self {
+            name: Default::default(),
+            metadata: [0; 128],
+            _reserved1: Default::default(),
+            queue_pubkey: Default::default(),
+            oracle_request_batch_size: Default::default(),
+            min_oracle_results: Default::default(),
+            min_job_results: Default::default(),
+            min_update_delay_seconds: Default::default(),
+            start_after: Default::default(),
+            variance_threshold: Default::default(),
+            force_report_period: Default::default(),
+            expiration: Default::default(),
+            consecutive_failure_count: Default::default(),
+            next_allowed_update_time: Default::default(),
+            is_locked: Default::default(),
+            crank_pubkey: Default::default(),
+            latest_confirmed_round: Default::default(),
+            current_round: Default::default(),
+            job_pubkeys_data: Default::default(),
+            job_hashes: Default::default(),
+            job_pubkeys_size: Default::default(),
+            jobs_checksum: Default::default(),
+            authority: Default::default(),
+            history_buffer: Default::default(),
+            previous_confirmed_round_result: Default::default(),
+            previous_confirmed_round_slot: Default::default(),
+            disable_crank: Default::default(),
+            job_weights: Default::default(),
+            creation_timestamp: Default::default(),
+            resolution_mode: Default::default(),
+            _ebuf: [0; 138],
+        }
+    }
+}
+
 #[zero_copy(unsafe)]
+#[derive(Default)]
 #[repr(packed)]
 pub struct SwitchboardDecimal {
     /// The part of a floating-point number that represents the significant digits of that number, and that is multiplied by the base, 10, raised to the power of scale to give the actual value of the number.
@@ -80,7 +121,21 @@ pub struct SwitchboardDecimal {
     pub scale: u32,
 }
 
+impl SwitchboardDecimal {
+    pub fn new(mantissa: i128, scale: u32) -> SwitchboardDecimal {
+        Self { mantissa, scale }
+    }
+    pub fn from_rust_decimal(d: Decimal) -> SwitchboardDecimal {
+        Self::new(d.mantissa(), d.scale())
+    }
+    pub fn from_f64(v: f64) -> SwitchboardDecimal {
+        let dec = Decimal::from_f64(v).unwrap();
+        Self::from_rust_decimal(dec)
+    }
+}
+
 #[zero_copy(unsafe)]
+#[derive(Default)]
 #[repr(packed)]
 pub struct AggregatorRound {
     /// Maintains the number of successful responses received from nodes.
@@ -115,6 +170,7 @@ pub struct AggregatorRound {
 }
 
 #[zero_copy(unsafe)]
+#[derive(Default)]
 #[repr(packed)]
 pub struct Hash {
     /// The bytes used to derive the hash.
@@ -122,6 +178,20 @@ pub struct Hash {
 }
 
 impl AggregatorAccountData {
+    pub fn new_from_bytes(data: &[u8]) -> Option<AggregatorAccountData> {
+        if data.len() < 8 {
+            return None;
+        }
+
+        if &data[..8] != &AggregatorAccountData::DISCRIMINATOR {
+            return None;
+        }
+
+        Some(*bytemuck::from_bytes(
+            &data[8..std::mem::size_of::<AggregatorAccountData>() + 8],
+        ))
+    }
+
     pub fn get_result(&self) -> Option<SwitchboardDecimal> {
         // Copy to avoid references to a packed struct
         let latest_confirmed_round_success = self.latest_confirmed_round.num_success;
